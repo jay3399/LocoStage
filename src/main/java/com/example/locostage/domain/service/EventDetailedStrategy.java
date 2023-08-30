b6bac9cdefa2139411d295ce433356747c5c8ceb
jay <joswxx@gmail.com>
2023-08-30 22:54:11 +0900

    상세페이지

--- src/main/java/com/example/locostage/domain/service/EventDetailedStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.example.locostage.domain.service;
-
-import com.example.locostage.application.dto.EventDetailed;
-
-public interface EventDetailedStrategy {
-
-    EventDetailed getEventDetail(Long eventId);
-
-}
