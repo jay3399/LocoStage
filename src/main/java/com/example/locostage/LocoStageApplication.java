0b4a9b04d343b2da0377457adf5cc3a1731f8830
jay <joswxx@gmail.com>
2023-08-30 15:07:48 +0900

    .

--- src/main/java/com/example/locostage/LocoStageApplication.java
+++ src/main/java/com/example/locostage/LocoStageApplication.java
@@ -3,6 +3,12 @@ package com.example.locostage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// 이메일이용 로그인서비스구현
+
+// 찜하기 기능이용시 , 알림기능 이용가능
+
+
 @SpringBootApplication
 public class LocoStageApplication {
 
